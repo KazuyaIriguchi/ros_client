ba8c720db8391b4744eaa38a8b3e49274c804a32
Kazuya Iriguchi <kazuya.iriguchi.j6k@jp.denso.com>
2023-07-05 10:59:23 +0900

    add: dropdown list to select parameter/topic/services

--- app.py
+++ app.py
@@ -7,32 +7,46 @@ def run_command(command):
     output, error = process.communicate()
     return output.decode("utf-8")
 
+def get_rosparams():
+    """Get list of ROS parameters"""
+    output = run_command("rosparam list")
+    return output.splitlines()
+
+def get_rostopics():
+    """Get list of ROS topics"""
+    output = run_command("rostopic list")
+    return output.splitlines()
+
+def get_rosservices():
+    """Get list of ROS services"""
+    output = run_command("rosservice list")
+    return output.splitlines()
+
 # Streamlit UI
 st.title("ROS Client Tool")
 
 # ROS Parameters
 st.subheader("Parameters")
-param_name = st.text_input("Enter Parameter Name")
-if st.button("Get Parameter"):
-    # Get and display the selected parameter here
-    command = f"rosparam get {param_name}"
+params = get_rosparams()
+selected_param = st.selectbox("Select Parameter", params)
+if st.button("Get Parameter Value"):
+    # Get and display the selected parameter value
+    command = f"rosparam get {selected_param}"
     output = run_command(command)
     st.write(f"Parameter Value: {output}")
 
 # ROS Topics
 st.subheader("Topics")
-if st.button("List Topics"):
-    # List ROS topics
-    command = "rostopic list"
-    output = run_command(command)
-    st.write("Topics:")
-    st.write(output)
+topics = get_rostopics()
+selected_topic = st.selectbox("Select Topic", topics)
+if st.button("Publish to Topic"):
+    # Publish to the selected topic here (you'll need to implement message publishing based on the topic type)
+    pass
 
 # ROS Services
 st.subheader("Services")
-if st.button("List Services"):
-    # List ROS services
-    command = "rosservice list"
-    output = run_command(command)
-    st.write("Services:")
-    st.write(output)
+services = get_rosservices()
+selected_service = st.selectbox("Select Service", services)
+if st.button("Call Service"):
+    # Call the selected service here (you'll need to implement service calling based on the service type)
+    pass
